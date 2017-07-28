f48395f0663deebbb9f72ed1e84a662c48e9d749
thelightbird <paul.maribon@gmail.com>
2017-07-28 13:10:12 +0200

    Auto cancel dialog when authentication fails

--- src/android/AuthRequestHandler.java
+++ src/android/AuthRequestHandler.java
@@ -3,24 +3,27 @@ package com.msopentech.authDialog;
 import org.apache.cordova.*;
 
 public class AuthRequestHandler extends CordovaPlugin {
- 
+
     public boolean onReceivedHttpAuthRequest(CordovaWebView view, final ICordovaHttpAuthHandler handler, String host, String realm) {
         AuthenticationDialog dialog = new AuthenticationDialog(cordova.getActivity(), host, realm);
 
-        dialog.setOkListener(new AuthenticationDialog.OkListener() {
-            public void onOk(String host, String realm, String username, String password) {
-                handler.proceed(username, password);
-            }
-        });
+        // dialog.setOkListener(new AuthenticationDialog.OkListener() {
+        //     public void onOk(String host, String realm, String username, String password) {
+        //         handler.proceed(username, password);
+        //     }
+        // });
+        //
+        // dialog.setCancelListener(new AuthenticationDialog.CancelListener() {
+        //     public void onCancel() {
+        //         handler.cancel();
+        //     }
+        // });
 
-        dialog.setCancelListener(new AuthenticationDialog.CancelListener() {
-            public void onCancel() {
-                handler.cancel();
-            }
-        });
+        // dialog.show();
 
-        dialog.show();
-        
+        // Auto cancel dialog when authentication fails
+        handler.cancel();
+
         return true;
     }
 }
